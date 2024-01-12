78eabeff093bdd1837f52fd8e05232ea35ff0891
shenxiang <shenxiang@foursmile001.com>
2024-01-12 13:13:28 +0800

    Initial Commit

---
+++ Canvas/CanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasApp.swift
+//  Canvas
+//
+//  Created by FS on 2024/1/12.
+//
+
+import SwiftUI
+
+@main
+struct CanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Canvas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Canvas
+//
+//  Created by FS on 2024/1/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
